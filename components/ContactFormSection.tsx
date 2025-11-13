dfd43671518e2b60a7d0abc1d5ff2c4ce7ead635
Alesson <g5-alesson@hotmail.com>
2025-11-13 14:03:48 -0300

    Atualização automática

--- components/ContactFormSection.tsx
+++ components/ContactFormSection.tsx
@@ -32,7 +32,7 @@ export default function ContactFormSection() {
                   name="contato-evoque" 
                   method="POST"
                   action="/contato-sucesso"
-                  netlify
+                  data-netlify="true"
                   data-netlify-honeypot="bot-field" 
                   className="space-y-6"
                 >
